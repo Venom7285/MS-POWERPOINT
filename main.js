e47b2a806241454b8bc1fc97776da74a1f2e2e06
Venom7285 <81704498+Venom7285@users.noreply.github.com>
2021-12-21 12:34:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+function save_s1_t() {
+    document.getElementById("out_s1_t").innerHTML = document.getElementById("slide_1_title").value;
+}
+
+function save_s1_s() {
+    document.getElementById("out_s1_s").innerHTML = document.getElementById("slide_1_subtitle").value;
+}
+
+function change_back_color1() {
+    document.getElementById("slide_number_one_div_out").style.backgroundColor 
+    = document.getElementById("b_c_s1").value;
+}
+
+function save_s2_t() {
+    document.getElementById("out_s2_t").innerHTML = document.getElementById("slide_2_title").value;
+}
+
+function save_s2_c() {
+    document.getElementById("out_s2_c").innerHTML = document.getElementById("slide_2_content").value;
+}
+
+function change_back_color2() {
+    document.getElementById("slide_number_two_div_out").style.backgroundColor 
+    = document.getElementById("b_c_s2").value;
+}
+
+function save_s3_t() {
+    document.getElementById("out_s3_t").innerHTML = document.getElementById("slide_3_title").value;
+}
+
+function save_s3_c() {
+    document.getElementById("out_s3_c").innerHTML = document.getElementById("slide_3_content").value;
+}
+
+function change_back_color3() {
+    document.getElementById("slide_number_three_div_out").style.backgroundColor 
+    = document.getElementById("b_c_s3").value;
+}
+
+function save_s4_t() {
+    document.getElementById("out_s4_t").innerHTML = document.getElementById("slide_4_title").value;
+}
+
+function save_s4_c() {
+    document.getElementById("out_s4_c").innerHTML = document.getElementById("slide_4_content").value;
+}
+
+function change_back_color4() {
+    document.getElementById("slide_number_four_div_out").style.backgroundColor 
+    = document.getElementById("b_c_s4").value;
+}
+
+function save_s5_t() {
+    document.getElementById("out_s5_t").innerHTML = document.getElementById("slide_5_title").value;
+}
+
+function change_back_color5() {
+    document.getElementById("slide_number_five_div_out").style.backgroundColor 
+    = document.getElementById("b_c_s5").value;
+}
